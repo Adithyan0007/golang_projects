68d0c58c0bd33dfdd2b851f3fe48c3b23aeb1f0a
adithyan <adithyan039@gmail.com>
2021-08-25 20:39:24 +0530

    added a multiple value returning program

---
+++ returning multiple values/multiple.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum, product := nums(10, 34)
+
+	fmt.Printf("sum is %v and product is %v",sum,product)
+}
+func nums(num1, num2 int) (int, int) {
+	sum := num1 + num2
+	product := num1 * num2
+
+	return sum, product
+
+}
